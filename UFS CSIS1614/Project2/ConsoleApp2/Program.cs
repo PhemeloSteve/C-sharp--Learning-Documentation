5ba7c4090937ed87ac512666a5dd98cfbf6319a0
Phemelo Mokwena <67952763+PhemeloSteve@users.noreply.github.com>
2023-03-10 09:13:45 +0200

    Add files via upload
    Projects for my Computer Sciences Projects during practical

---
+++ UFS CSIS1614/Project2/ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Phemelo Mokwena
+//2023129683
+//Practical 1
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class CProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Phemelo Mokwena and my student number is 2023129683");
+            Console.WriteLine("   /\\/\\");
+            Console.WriteLine("I  \\  /  Progamming!");
+            Console.WriteLine("    \\/");
+            Console.WriteLine("Press any key to close the program...");
+            Console.ReadKey();
+
+        }
+    }
+}
