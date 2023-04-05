7bdbf3214ff0200e33c2554b1ccbc3960071391d
Phemelo Mokwena <67952763+PhemeloSteve@users.noreply.github.com>
2023-04-05 10:45:42 +0200

    Create ForLoopTest.cs

---
+++ ForLoopTest.cs
@@ -0,0 +1,16 @@
+/*
+In this program we will be making use of for loop for a basic cointing from 0 to 4
+*/
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        for (int i = 0; i<5; i++ )
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
