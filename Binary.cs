2fec335054a039828610f8fb4267b098e1ad953e
Phemelo Mokwena <67952763+PhemeloSteve@users.noreply.github.com>
2023-04-20 13:00:37 +0200

    Update Binary.cs

--- Binary.cs
+++ Binary.cs
@@ -10,24 +10,29 @@ namespace Binary
     {
         public static void Main()
         {
+            // Declaring decimal int variable and string for the sBinary to store our binary in
+            
             int i;
             string num;
             string sBinary = "";
 
+            // Taking user input for decimal integer value we want to convert to binary number
+            
             Console.Write("Enter a decimal number that you want to convert to binary number: ");
             num = Console.ReadLine();
 
 
             i = int.Parse(num);
 
-            // Calculating the binary using while loop
+            // Calculating the binary using do-while loop
             do
             {
-                sBinary= i%2 + sBinary;
+                sBinary= i%2 + sBinary; // adding the remainder in a revers order
                 i/=2;
             }
             while(i != 0);
-
+            
+            // Displaying message
             Console.WriteLine("\nThe binary number of the decimal number " + num + " is " + sBinary + ".");
 
             // End of a code
