b16b88381e2ebafe45a89ea01f17992ff362670d
Phemelo Mokwena <67952763+PhemeloSteve@users.noreply.github.com>
2023-05-10 11:32:52 +0200

    Add files via upload

---
+++ TestCodes/Factorial/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iNum;
+            string sNum;
+
+
+            bool isSuccess;
+            Console.Write("Enter an integer that you want a number of: ");
+            sNum = Console.ReadLine();
+
+            isSuccess = int.TryParse(sNum, out iNum);
+
+            if (isSuccess && iNum >= 0)
+            {
+                int iFactorial = Fact(iNum);
+                Console.WriteLine(iFactorial);
+            }
+            else
+            {
+                Console.WriteLine("Invalid user input");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+
+        public static int Fact(int _iNum)
+        {
+            if (_iNum > 1)
+            {
+                return _iNum * Fact(_iNum - 1);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
